9b5c76a874bd33a24bb316009d7067fa6346f307
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-05-10 16:57:08 +0400

    first commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+
+pip install --upgrade pippip install --force-reinstall -U setuptools
+cd django_sms
+
+pip install -r requirements.txt
+
+if [[ $CREATE_SUPERUSER ]]
+then
+python manage.py createsuperuser --noinput --username $SUPERUSER_NAME --email $SUPERUSER_EMAIL
+fi
+ 
---
+++ django_sms/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install --upgrade pippip install --force-reinstall -U setuptools
+
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]
+then 
+python manage.py createsuperuser --no-input
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='$SUPERUSER_EMAIL').update(password='$SUPERUSER_PASSWORD')"
+fi
+
