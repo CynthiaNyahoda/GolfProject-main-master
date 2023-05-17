9f7e19b97518a3922173f0ea95840ba76e4806a8
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-05-17 15:42:12 +0400

    try again

--- django_sms/django_sms/settings.py
+++ django_sms/django_sms/settings.py
@@ -162,5 +162,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='default_value')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
