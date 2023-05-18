92ace1095ebf8485114a4b7cb301ce334de6f4fd
github username <t.gunness35@gmail.com>
2023-05-18 22:41:02 +0400

    urls fixed request.scheme

--- django_sms/smsApp/views.py
+++ django_sms/smsApp/views.py
@@ -383,7 +383,7 @@ def save_member(request):
             member = saved_member
 
             # Get the base URL or domain
-            base_url = request.request.scheme + ':/' + request.get_host()
+            base_url = request.scheme + '://' + request.get_host()
 
             # Call the send_member_email function
             send_member_email(member, base_url)
