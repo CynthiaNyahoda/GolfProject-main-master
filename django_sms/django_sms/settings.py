0ab6797a6f73ad5259bc47454865bf3207da5ac8
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-05-18 13:07:21 +0400

    debug false

--- django_sms/django_sms/settings.py
+++ django_sms/django_sms/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "" "django.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
