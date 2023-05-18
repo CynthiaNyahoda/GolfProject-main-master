091ae1d56d5fe5dc3a5db3b3f95b57391db2291f
CynthiaNyahoda <cynthianyahoda19@gmail.com>
2023-05-18 12:56:43 +0400

    update build/bash

--- django_sms/build.sh
+++ django_sms/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+python -m pip install --upgrade pip
+
 pip install --upgrade pippip install --force-reinstall -U setuptools
-python manage.py makemigrations
-python manage.py migrate
+cd django_sms
+
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-
+ 
